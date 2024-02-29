c970f8ac6697d065dfa92d55761b73dddd8c6da1
lowfabi <fabian.geiblinger@gmail.com>
2024-02-29 17:07:37 +0100

    added a method for transforming

--- src/transform.py
+++ src/transform.py
@@ -1,22 +1,27 @@
 import cv2
 import numpy as np
 
-# Load the image
-image = cv2.imread('../input/boden.jpg')
-width = 502
-height = 743
-# Specify the coordinates of the four corners of the trapezoid
-# The order is: top-left, top-right, bottom-right, bottom-left
-src_pts = np.array([[203, 66], [438, 76], [570, 396], [68, 396]], dtype="float32")
+def transform_img_to_rectangle(width, height, source_points, destination_pts):
+    """
+    Transforms the perspective of an image from a trapezoid to a rectangle.
 
-# Specify the coordinates of the rectangle you're transforming into
-# This would typically be a straight rectangle, so we choose points accordingly
-# The order should match the trapezoid's points: top-left, top-right, bottom-right, bottom-left
-dst_pts = np.array([[0, 0], [width - 1, 0], [width - 1, height - 1], [0, height - 1]], dtype="float32")
+    Parameters:
+    - width: The width of the output image.
+    - height: The height of the output image.
+    - src_pts: A list of points defining the corners of the trapezoid in the input image.
+    - dst_pts: A list of points defining the corners of the rectangle in the output image.
+    """
 
-# Calculate the perspective transform matrix
-M = cv2.getPerspectiveTransform(src_pts, dst_pts)
+    INPUT_PATH = '../input/boden.jpg'
+    image = cv2.imread('../input/boden.jpg')
+    
+    if image is None:
+        print(f"Error: Could not load image from {INPUT_PATH}")
+        return
 
-# Apply the perspective transformation
-transformed = cv2.warpPerspective(image, M, (width, height))
-cv2.imwrite('../output/wrappedimage.png',transformed)
+    # Calculate the perspective transform matrix
+    M = cv2.getPerspectiveTransform(source_points, destination_pts)
+
+    # Apply the perspective transformation
+    transformed = cv2.warpPerspective(image, M, (width, height))
+    cv2.imwrite('../output/wrappedimage.png',transformed)
